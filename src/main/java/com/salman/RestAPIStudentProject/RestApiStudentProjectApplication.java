b79680db4a73ebbd517ed561b1bb7ae8a014c542
MohamedSalmanKhan1 <mohamed.khan@capestart.com>
2021-02-15 18:15:09 +0530

    Student project integrated with DB

--- src/main/java/com/salman/RestAPIStudentProject/RestApiStudentProjectApplication.java
+++ src/main/java/com/salman/RestAPIStudentProject/RestApiStudentProjectApplication.java
@@ -3,7 +3,7 @@ package com.salman.RestAPIStudentProject;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication (scanBasePackages={"com.salman.RestAPIStudentProject"})
+@SpringBootApplication(scanBasePackages = { "com.salman.restapistudentproject" })
 public class RestApiStudentProjectApplication {
 
 	public static void main(String[] args) {
